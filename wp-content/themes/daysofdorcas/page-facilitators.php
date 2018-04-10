2aa327075b027d813433d908fc22ba1628b208e1
Tobby <Tobby@Corporates-iMac.local>
2018-04-10 14:39:07 +0100

    page-enquiries

--- wp-content/themes/daysofdorcas/page-facilitators.php
+++ wp-content/themes/daysofdorcas/page-facilitators.php
@@ -5,12 +5,10 @@ Template Name: Days of Dorcas
 ?>
 <?php include('custom-header.php') ?>
        
-        <section class="MeetFacilities"> 
-       
+<section class="MeetFacilities">        
          <!-- title -->
             <h2>Meet The Faciltitators</h2>
-
-        <section> <!-- list of facilitators -->
+    <section> <!-- list of facilitators -->
         <article  class="facilitator">
                 <img src="" class="profile-avatar-big">
                 <p class="fac-name">Bénédicte Kurzen</p>
@@ -24,8 +22,7 @@ Template Name: Days of Dorcas
                 <img src="" class="profile-avatar-big">
                 <p class="fac-name">Jumoke Sanwo</p>
                 <p>Jumoke is a visual artist out of Lagos, Nigeria, she holds a Bachelor of Arts (Hons.) in English Studies from the Obafemi Awolowo University. She is a self-taught photographer and uses the medium in conjunction with, text, Video-Art and Virtual Reality as her means of expression. </p>
-
-<p>Her works address aesthetic concerns, with a focus on enlightenment, religion, technology and mobility, which fosters new narratives on re-imaging the African Continent in the 21st Century.  </p>
+                <p>Her works address aesthetic concerns, with a focus on enlightenment, religion, technology and mobility, which fosters new narratives on re-imaging the African Continent in the 21st Century.  </p>
         </article> 
 
 
@@ -33,32 +30,27 @@ Template Name: Days of Dorcas
                 <img src="" class="profile-avatar-big">
                 <p class="fac-name">TY Bello</p>
                 <p>Toyin Sokefun-Bello better known as TY Bello, is a Nigerian singer, songwriter, å and philanthropist. Prior to pursuing a solo career, she was a member of the defunct gospel band Kush. </p>
-<p>TY Bello was born in Ogun State. She attained a degree in economics from the University of Lagos. She briefly practised journalism and eventually ventured into photography.  </p>
+                <p>TY Bello was born in Ogun State. She attained a degree in economics from the University of Lagos. She briefly practised journalism and eventually ventured into photography.  </p>
         </article> 
 
          <article  class="facilitator">
                 <img src="" class="profile-avatar-big">
                 <p class="fac-name">Uche Okpa-Iroha</p>
                 <p>Toyin Sokefun-Bello better known as TY Bello, is a Nigerian singer, songwriter, å and philanthropist. Prior to pursuing a solo career, she was a member of the defunct gospel band Kush. </p>
-
-<p>TY Bello was born in Ogun State. She attained a degree in economics from the University of Lagos. She briefly practised journalism and eventually ventured into photography.  </p>
-              
+                <p>TY Bello was born in Ogun State. She attained a degree in economics from the University of Lagos. She briefly practised journalism and eventually ventured into photography.  </p>  
         </article> 
 
          <article  class="facilitator">
                 <img src="" class="profile-avatar-big">
                 <p class="fac-name">Yetunde Ayeni-Babaeko</p>
                 <p>Yetunde Ayeni-Babaeko was born in Enugu, Eastern Nigeria in October 1978.However, it was in Germany that her schooling began. After her Abitur (A levels) she did her photography a.</p>
-
-<p>On completing her apprenticeship, Africa beckoned and she returned to Nigeria and joined Ess-Ay Studio for a year programme, (facilitated by Invent, Germany between 2003 and 2004).  </p>
+                <p>On completing her apprenticeship, Africa beckoned and she returned to Nigeria and joined Ess-Ay Studio for a year programme, (facilitated by Invent, Germany between 2003 and 2004).  </p>
         </article> 
 
-       
-
-        </section> 
+    </section> 
         <!-- close list facilitators -->
 
-        </section> 
+</section> 
         <!-- close meet the facilitators -->
 
 
